f51a920a745bc0cb746fc8d967937a4e0a07214e
alvesmarcolas <alvesbrunom8@gmail.com>
2024-03-19 11:20:03 -0300

    Responsividade - 60%

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Portfolio from './components/portfolio/Portfolio';
 import Footer from './components/footer/Footer';
 
 import './index.css'
+import './medias.css'
 
 function App() {
   return (
